0462b64bb7f208865d7a0db12a06eb51583feead
Masaya Kumagai <fire156flow@gmail.com>
2021-10-05 22:17:08 +0900

    implement testmodepage

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,9 @@ class _TopPage extends State<MyHomePage>{
   }
   Widget build(BuildContext context){
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("TopPage"),
+      ),
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
@@ -69,7 +72,7 @@ class _TopPage extends State<MyHomePage>{
             textStyle: const TextStyle(fontSize: 20),
           ),
           onPressed: (){
-            Navigator.push(context,MaterialPageRoute(builder: (context) => SelectDiffMenu()));
+            Navigator.push(context,MaterialPageRoute(builder: (context) => SelectDiffMenu(title:widget.title)));
           },
           child: const Text('Test Mode'),
         ),
@@ -88,8 +91,9 @@ class _TopPage extends State<MyHomePage>{
 }
 
 class SelectDiffMenu extends StatefulWidget{
-  SelectDiffMenu({Key key}) : super(key: key);
+  SelectDiffMenu({Key key,this.title}) : super(key: key);
 
+  final String title;
   _SelectDiffMenu createState() => _SelectDiffMenu();
 }
 
@@ -189,6 +193,7 @@ class _SelectDiffMenu extends State<SelectDiffMenu>{
             ),
             onTap: () {
               print(data[index]);
+              Navigator.push(context,MaterialPageRoute(builder: (context) => TestModePageState(title: widget.title,settings:data[index])));
             },
           );
         },
@@ -202,6 +207,7 @@ class FreeModePageState extends StatefulWidget{
   FreeModePageState({Key key,this.title}) : super(key: key);
 
   final String title;
+
   _FreeModePageState createState() => _FreeModePageState();
 
 }
@@ -367,7 +373,7 @@ class _FreeModePageState extends State<FreeModePageState> {
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   Exam_Retry(showTime, showCount, digits);
                 },
@@ -375,7 +381,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "RETRY",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   Exam_Start(showTime, showCount, digits);
                 },
@@ -397,7 +403,7 @@ class _FreeModePageState extends State<FreeModePageState> {
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(7);
                 },
@@ -405,7 +411,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "7",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(8);
                 },
@@ -413,7 +419,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "8",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(9);
                 },
@@ -426,7 +432,7 @@ class _FreeModePageState extends State<FreeModePageState> {
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(4);
                 },
@@ -434,7 +440,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "4",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(5);
                 },
@@ -442,7 +448,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "5",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(6);
                 },
@@ -455,7 +461,7 @@ class _FreeModePageState extends State<FreeModePageState> {
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(1);
                 },
@@ -463,7 +469,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "1",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(2);
                 },
@@ -471,7 +477,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "2",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(3);
                 },
@@ -484,7 +490,7 @@ class _FreeModePageState extends State<FreeModePageState> {
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(0);
                 },
@@ -492,7 +498,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "0",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   input_answer(-1);
                 },
@@ -500,7 +506,7 @@ class _FreeModePageState extends State<FreeModePageState> {
                   "AC",
                 ),
               ),
-              FlatButton(
+              TextButton(
                 onPressed: () {
                   correctness_decision();
                 },
@@ -526,7 +532,6 @@ class _FreeModePageState extends State<FreeModePageState> {
             tooltip: 'Settings',
             onPressed: () {
               Navigator.push(context,MaterialPageRoute(builder: (context) => SettingsPage()));
-
             },
           ),
         ],
@@ -541,8 +546,356 @@ class _FreeModePageState extends State<FreeModePageState> {
       ),
     );
   }
+}
+
+class TestModePageState extends StatefulWidget{
+  TestModePageState({Key key,this.title,this.settings}) : super(key: key);
+
+  final String title;
+  final List settings;
+  _TestModePageState createState() => _TestModePageState();
 
 }
+
+class _TestModePageState extends State<TestModePageState> {
+
+  static String display = 'Press Start';
+
+  static TextEditingController _textSection_controller;
+  static bool TextEditing = true;
+
+  static double showTime = 0.30;
+  static int showCount = 5;
+  static int digits = 2;
+
+  static int input_answer_num = 0;
+
+  static List<int> preDisplay = [];
+
+
+
+  void initState() {
+    super.initState();
+    _textSection_controller = new TextEditingController(text: display);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    Future<void> Exam_Start(double showTime,int showCount,int digits) async {
+
+      // SharedPreferences prefs = await SharedPreferences.getInstance();
+      int test_grade = (widget.settings[0]).toInt();
+      int test_digits = (widget.settings[1]).toInt();
+      int test_count = (widget.settings[2]).toInt();
+      double test_totaltime = (widget.settings[3]).toDouble();
+
+      double showTime;
+      int showCount;
+      int digits;
+
+      showTime = (test_totaltime / test_count);
+      showCount = test_count;
+      digits = test_digits;
+      print(showTime);
+
+      // if(prefs.containsKey('showTimeValue')){
+      //   showTime = double.parse(prefs.getString('showTimeValue'));
+      // }else{
+      //   showTime = 0.3;
+      // }
+      // if(prefs.containsKey('showCountValue')){
+      //   showCount = int.parse(prefs.getString('showCountValue'));
+      // }else{
+      //   showCount = 5;
+      // }
+      // if(prefs.containsKey('digitsValue')){
+      //   digits = int.parse(prefs.getString('digitsValue'));
+      // }else{
+      //   digits = 2;
+      // }
+
+      if(TextEditing == false){
+        return;
+      }
+
+      TextEditing = false;
+      input_answer_num = 0;
+
+      preDisplay = [];
+      int showTime_int = (showTime * 1000).toInt();
+      var random = new math.Random();
+      for(int i=0;i<showCount;i++){
+        var num = random.nextInt(math.pow(10,digits) - math.pow(10,digits-1) + 1);
+        num = num + math.pow(10,digits-1) - 1;
+        // log(showTime_int.toString());
+        preDisplay.add(num);
+      }
+
+      _textSection_controller.text = '';
+      await Future.delayed(Duration(milliseconds: showTime_int));
+
+      for(int i=0;i<showCount;i++){
+        _textSection_controller.text = preDisplay[i].toString();
+        await Future.delayed(Duration(milliseconds: showTime_int));
+      }
+      _textSection_controller.text = '';
+      // log(preDisplay.toString());
+
+      TextEditing = true;
+
+    }
+
+    Future<void> Exam_Retry(double showTime,int showCount,int digits) async {
+
+      if(TextEditing == false){
+        return;
+      }
+
+      TextEditing = false;
+      input_answer_num = 0;
+
+      int showTime_int = (showTime * 1000).toInt();
+
+      _textSection_controller.text = '';
+      await Future.delayed(Duration(milliseconds: showTime_int));
+
+      for(int i=0;i<preDisplay.length;i++){
+        _textSection_controller.text = preDisplay[i].toString();
+        await Future.delayed(Duration(milliseconds: showTime_int));
+      }
+      _textSection_controller.text = '';
+      // log(preDisplay.toString());
+
+      TextEditing = true;
+
+    }
+
+    void input_answer(int input){
+
+      if(TextEditing == false){
+        return;
+      }
+
+      if(0 <= input && input <= 9){
+
+        input_answer_num = input_answer_num * 10;
+        input_answer_num = input_answer_num + input;
+        _textSection_controller.text = input_answer_num.toString();
+        // log(input_answer_num.toString());
+
+      }else if(input == -1){
+
+        input_answer_num = 0;
+        _textSection_controller.text = '';
+
+      }
+    }
+
+    bool correctness_decision(){
+      int correct_num = preDisplay.reduce((a, b) => a + b);
+
+      log('input=${correct_num.toString()},correct=${input_answer_num.toString()}');
+      if(correct_num == input_answer_num){
+        _textSection_controller.text = '〇';
+        return true;
+      }else{
+        _textSection_controller.text = correct_num.toString();
+        return false;
+      }
+    }
+
+    Widget textSection = Container(
+        padding: const EdgeInsets.all(32),
+        child: TextField(
+          obscureText: false,
+          controller: _textSection_controller,
+          readOnly: true,
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: '',
+          ),
+          style: TextStyle(
+              fontSize: 50
+          ),
+        )
+    );
+
+    final controlSection = Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Exam_Retry(showTime, showCount, digits);
+                },
+                child: Text(
+                  "RETRY",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Exam_Start(showTime, showCount, digits);
+                },
+                child: Text(
+                  "START",
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+
+    final inputSection = Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton(
+                onPressed: () {
+                  input_answer(7);
+                },
+                child: Text(
+                  "7",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  input_answer(8);
+                },
+                child: Text(
+                  "8",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  input_answer(9);
+                },
+                child: Text(
+                  "9",
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton(
+                onPressed: () {
+                  input_answer(4);
+                },
+                child: Text(
+                  "4",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  input_answer(5);
+                },
+                child: Text(
+                  "5",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  input_answer(6);
+                },
+                child: Text(
+                  "6",
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton(
+                onPressed: () {
+                  input_answer(1);
+                },
+                child: Text(
+                  "1",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  input_answer(2);
+                },
+                child: Text(
+                  "2",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  input_answer(3);
+                },
+                child: Text(
+                  "3",
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton(
+                onPressed: () {
+                  input_answer(0);
+                },
+                child: Text(
+                  "0",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  input_answer(-1);
+                },
+                child: Text(
+                  "AC",
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  correctness_decision();
+                },
+                child: Text(
+                  "ANS",
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+
+        title: Text(widget.title),
+
+      ),
+      body: Column(
+        children: [
+          textSection,
+          controlSection,
+          inputSection,
+        ],
+      ),
+    );
+  }
+}
+
 
 class SettingsPage extends StatefulWidget{
   SettingsPage({Key key}) : super(key: key);
